2ef751fa0f659e8dbada2d7d8db870ba51d39554
prakash126 <prakashkumar845411@gmail.com>
2020-09-22 19:38:50 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+declare var $:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'iframehtml';
+
+  fun1(){
+    $('#div1').css('width','100%').css('height','100%');
+  }
 }
